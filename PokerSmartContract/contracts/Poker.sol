b45caf269a1cc9681bb9854a34320f5c8721570e
MozartofCode <bertan.berker@gmail.com>
2024-06-20 05:19:28 -0400

    Created the setters

--- PokerSmartContract/contracts/Poker.sol
+++ PokerSmartContract/contracts/Poker.sol
@@ -17,11 +17,40 @@ contract Poker {
     Player player1;
     Player player2;
 
+    // This is the constructor for the contract
     constructor(uint256 _balance1, uint256 _balance2) {
         player1.balance = _balance1;
         player2.balance = _balance2;
     }
 
+
+    // These are the setters for the contract
+    function setPlayer1Balance(uint256 _balance) public {
+        player1.balance = _balance;
+    }
+
+    function setPlayer2Balance(uint256 _balance) public {
+        player2.balance = _balance;
+    }
+
+    function setPlayer1CurrentBet(uint256 _bet) public {
+        player1.currentBet = _bet;
+    }
+
+    function setPlayer2CurrentBet(uint256 _bet) public {
+        player2.currentBet = _bet;
+    }
+
+    function setPlayer1Hand(string[] memory _hand) public {
+        player1.hand = _hand;
+    } 
+
+    function setPlayer2Hand(string[] memory _hand) public {
+        player2.hand = _hand;
+    } 
+
+
+    // These are the getters for the contract
     function getPlayer1Balance() public view returns (uint256){
         return player1.balance;
     }
