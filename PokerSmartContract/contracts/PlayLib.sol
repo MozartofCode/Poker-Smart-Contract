27038adf714575c1469cb53754cec3108fcb43a4
MozartofCode <bertan.berker@gmail.com>
2024-06-22 10:46:46 -0400

    Rank helpers are complete

--- PokerSmartContract/contracts/PlayLib.sol
+++ PokerSmartContract/contracts/PlayLib.sol
@@ -96,8 +96,45 @@ library PlayLib{
 	
 	// Helper Functions for getting rankings
 	function isRoyalFlush(CardLib.Card[] memory _hand, CardLib.Card[] memory _table) public pure returns (bool) {
-		return true;
+		CardLib.Card[] memory allCards = new CardLib.Card[](_hand.length + _table.length);
+        for (uint i = 0; i < _hand.length; i++) {
+            allCards[i] = _hand[i];
+        }
+        for (uint i = 0; i < _table.length; i++) {
+            allCards[_hand.length + i] = _table[i];
+        }
+
+        string[] memory royalRanks = new string[](5);
+        royalRanks[0] = "10";
+        royalRanks[1] = "Jack";
+        royalRanks[2] = "Queen";
+        royalRanks[3] = "King";
+        royalRanks[4] = "Ace";
+
+        for (uint i = 0; i < 4; i++) { // Check each suit
+            string memory suit = ["Diamonds", "Spades", "Clubs", "Hearts"][i];
+            uint matchCount = 0;
+
+            for (uint j = 0; j < allCards.length; j++) {
+                for (uint k = 0; k < royalRanks.length; k++) {
+                    if (
+                        keccak256(abi.encodePacked(allCards[j].rank)) == keccak256(abi.encodePacked(royalRanks[k])) &&
+                        keccak256(abi.encodePacked(allCards[j].suit)) == keccak256(abi.encodePacked(suit))
+                    ) {
+                        matchCount++;
+                        break;
+                    }
+                }
+            }
+
+            if (matchCount == 5) {
+                return true;
+            }
+        }
+
+        return false;
 	}
+
 
 	function isStraightFlush(CardLib.Card[] memory _hand, CardLib.Card[] memory _table) public pure returns (bool) {
 		return isFlush(_hand, _table) && isStraight(_hand, _table);
@@ -173,9 +210,36 @@ library PlayLib{
 	}
 	
 	function isStraight(CardLib.Card[] memory _hand, CardLib.Card[] memory _table) public pure returns (bool) {
-		return true;
+    	string[13] memory rankOrder = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+        uint8[13] memory rankValues;
+
+        for (uint8 i = 0; i < rankOrder.length; i++) {
+            rankValues[i] = i + 2;
+        }
+		
+		string[7] memory ranks;
+		ranks[0] = _hand[0].rank;
+		ranks[1] = _hand[1].rank;
+		ranks[2] = _table[0].rank;
+		ranks[3] = _table[1].rank;
+		ranks[4] = _table[2].rank;
+		ranks[5] = _table[3].rank;
+		ranks[6] = _table[4].rank;
+
+		uint8[7] AllRankValues;		
+		        
+        for (uint i = 0; i < ranks.length; i++) {
+            for (uint8 j = 0; j < rankOrder.length; j++) {
+                if (keccak256(abi.encodePacked(ranks[i])) == keccak256(abi.encodePacked(rankOrder[j]))) {
+                    allRankValues[i] = rankValues[j];
+                }
+            }
+        }
+		
+        allRankValues = sortRanks(allRankValues);
+        return checkConsecutive(allRankValues);
 	}
-	
+
 	function isThreeOfaKind(CardLib.Card[] memory _hand, CardLib.Card[] memory _table) public pure returns (bool) {
 		string[7] memory ranks;
 		ranks[0] = _hand[0].rank;
@@ -217,7 +281,7 @@ library PlayLib{
 			for (uint j = i+1; j < ranks.length; j++) {
 				if (keccak256(abi.encodePacked(ranks[i])) == keccak256(abi.encodePacked(ranks[j]))) {	
 					pairCount += 1;
-					
+
 					if (pairCount == 1) {
 						pairs = ranks[i];
 					}
@@ -258,6 +322,52 @@ library PlayLib{
 		return false;
 	}
 
+
+	// Miscaleneous Helpers
+	function sortRanks(uint8[] memory _ranks) internal pure returns (uint8[] memory) {
+        for (uint i = 0; i < _ranks.length; i++) {
+            for (uint j = i + 1; j < _ranks.length; j++) {
+                if (_ranks[i] > _ranks[j]) {
+                    uint8 temp = _ranks[i];
+                    _ranks[i] = _ranks[j];
+                    _ranks[j] = temp;
+                }
+            }
+        }
+        return _ranks;
+    }
+
+
+    function checkConsecutive(uint8[] memory _ranks) internal pure returns (bool) {
+        uint consecutiveCount = 1;
+        for (uint i = 1; i < _ranks.length; i++) {
+            if (_ranks[i] == _ranks[i - 1]) {
+                continue;
+            } else if (_ranks[i] == _ranks[i - 1] + 1) {
+                consecutiveCount++;
+                if (consecutiveCount == 5) {
+                    return true;
+                }
+            } else {
+                consecutiveCount = 1;
+            }
+        }
+
+        // Special case for Ace to Five straight
+        if (_ranks[_ranks.length - 1] == 14) {
+            consecutiveCount = 1;
+            for (uint i = 0; i < _ranks.length - 1; i++) {
+                if (_ranks[i] == 5 - i) {
+                    consecutiveCount++;
+                    if (consecutiveCount == 5) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 
 
 }
