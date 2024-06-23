77872d428cb029848e85b9a44ca7fedb77bbd23b
MozartofCode <bertan.berker@gmail.com>
2024-06-23 05:32:04 -0400

    Created the python files

---
+++ CLI.py
@@ -0,0 +1,10 @@
+# @Author: Bertan Berker
+# @Filename: CLI.py
+# This file is the command Line Interface that interacts with the smart contract and 
+# sets the game between the user and the poker bot
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ PokerBot.py
@@ -0,0 +1,8 @@
+# @Author: Bertan Berker
+# @Filename: PokerBot.py
+# This file has the functionality of my poker bot
+
+class MyPokerBot:
+    def __init__(self):
+        return
+    
